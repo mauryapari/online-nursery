c5c0f57cf032e5e059f46aefb602b077eaaac6d0
Paritosh Maurya <paritosh.maurya@tadigital.com>
2021-10-23 23:57:54 +0530

    Removing commented code

--- src/components/util-content/section-header/SectionHeader.vue
+++ src/components/util-content/section-header/SectionHeader.vue
@@ -13,13 +13,11 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-// @import url('https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap');
 
 .section-heading {
     text-align: center;
     font-size: 40px;
     line-height: 1.4;
     @include ralewayRegular;
-    // font-family: 'Raleway';
 }
 </style>
